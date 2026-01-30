f2c95379d340b8ec475da6a05ce9975b5a1b4193
parvizorzu <parviziorzukul001@gmail.com>
2026-01-31 00:23:10 +0500

    db.py

--- gaia_project/db.py
+++ gaia_project/db.py
@@ -1,6 +1,10 @@
 import duckdb
 
 def execute_query(query_path):
+    """
+    Загружает SQL-запрос из файла, выполняет его в БД DuckDB 
+    и возвращает результат в виде объекта Pandas DataFrame.
+    """
     con = duckdb.connect('gaia_project/my.db', read_only=True)
     
     with open(query_path, 'r') as f:
