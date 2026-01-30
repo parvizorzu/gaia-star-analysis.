da232c6feca06b4a1782888eb3363a4f0f9b772e
parvizorzu <parviziorzukul001@gmail.com>
2026-01-31 00:04:04 +0500

    db.py

--- gaia_project/db.py
+++ gaia_project/db.py
@@ -1,7 +1,7 @@
 import duckdb
 
 def execute_query(query_path):
-    con = duckdb.connect('my.db')
+    con = duckdb.connect('gaia_project/my.db', read_only=True)
     
     with open(query_path, 'r') as f:
         sql_script = f.read()
